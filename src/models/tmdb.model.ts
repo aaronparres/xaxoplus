656e78d54c02cc27db51298693fad0bd0d981271
Aarón Parres López <aaronplmultimedia@gmail.com>
2021-07-17 23:34:50 +0200

    Add data models file for future responses

---
+++ src/models/tmdb.model.ts
@@ -0,0 +1,6 @@
+import {
+  DiscoverMovieResponse as PopularMoviesResponse,
+  DiscoverTvResponse as PopularSeriesResponse,
+} from 'moviedb-promise/dist/request-types';
+
+export type { PopularMoviesResponse, PopularSeriesResponse };
