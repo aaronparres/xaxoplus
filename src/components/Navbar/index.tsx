061a5539f3a9414f3f6cac901dbdd98acc215049
Aarón Parres López <aaronplmultimedia@gmail.com>
2021-07-19 14:20:10 +0200

    Show navbar background onScroll

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { ReactComponent as Logo } from 'assets/svg/logo.svg';
@@ -6,8 +7,24 @@ import { faSearch } from '@fortawesome/free-solid-svg-icons';
 import styles from './styles.module.scss';
 
 export default function Header() {
+  const [showTab, setShowTab] = useState(false);
+
+  useEffect(() => {
+    function hideOnScroll() {
+      if (window.scrollY > 5) {
+        setShowTab(true);
+      } else {
+        setShowTab(false);
+      }
+    }
+    window.addEventListener('scroll', hideOnScroll);
+    return () => {
+      window.removeEventListener('scroll', hideOnScroll);
+    };
+  }, []);
+
   return (
-    <nav className={styles.nav}>
+    <nav className={`${styles.nav} ${showTab && styles.showTab}`}>
       <Link to="/home">
         <Logo className={styles.logo} />
       </Link>
